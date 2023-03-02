d230cc218de94591bcb8b05c99db7a76c6243f88
DaliaDa <dalia.dauciuniene@gmail.com>
2023-03-02 13:10:44 +0200

    2 Programs

--- Tasks.py
+++ Tasks.py
@@ -1,3 +1,7 @@
+#Write a program to print out all the numbers from 0 to 100 that are divisible by 7
+for x in range(0, 100, 7):
+    print(x)
+
 #Write a program in Python to reverse a word. Words should be provided using input function.
 txt = input("Text here: ") [::-1]
 print (txt)
